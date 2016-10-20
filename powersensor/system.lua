eafdb9619dbcf4bc864af99f88cfdc44cce67e2d
David Precious <davidp@preshweb.co.uk>
2016-10-20 12:24:23 +0100

    Calculate average current in watts, too.

--- powersensor/system.lua
+++ powersensor/system.lua
@@ -1,7 +1,8 @@
-
-
+-- Use a timer to check the light level, and decide if we're seeing
+-- the sartrt of a new flash, increment our flash counter if so
 in_flash = false
 flash_count = 0
+flashes_since = tmr.time()
 function checkValue()
     -- print("Checkvalue called")
     value = adc.read(0)
@@ -31,22 +32,20 @@ print("registered timer")
 -- darts, so if it works, it's good enough for now!
 -- In fact, I finished it and flashed it after returning from the pub,
 -- somewhat pissed, so I deserve whatever I get for flashing this...
-print("Setting up web server")
 srv = net.createServer(net.TCP)
 srv:listen(80, function(conn)
     conn:on('receive', function(client,request)
         print('Received a request')
-	-- on my meter, it's 1000 flashes per KWh used; if that differs
-
-
-
-
--- If the input is high, and was not high before, it's the start of a new pulse - fire HTTP request
-
-        -- for your meter, change this to suit
-	usage_kwh = flash_count * 0.001;
-        flash_count = 0;
-        client:send("{usage_kwh:" .. usage_kwh .. "}")
+	usage_kwh = flash_count * 0.001
+	period_s = tmr.time() - flashes_since
+        flash_count = 0
+        flashes_since = tmr.time()
+        -- work out the average current in watts - watt-hours / hours
+	avg_watts = (usage_kwh * 1000) / (period_s * 60 * 60)
+        client:send(
+            '{"usage_kwh":' .. usage_kwh .. ',"avg_watts":' .. avg_watts
+            .. ',"seconds":' .. period_s..'}'
+        )
         client:close()
     end)
 end)
